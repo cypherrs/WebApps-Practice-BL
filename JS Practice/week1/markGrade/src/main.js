da58824e56b35f0c3e7781f97fa538bfcb70ba84
Rupesh R Sahoo <rs0722@srmist.edu.in>
2025-05-26 14:03:01 +0530

    do

---
+++ JS Practice/week1/markGrade/src/main.js
@@ -0,0 +1,14 @@
+let mark = prompt("Enter your marks");
+if (mark >= 80 && mark <= 100) {
+  console.log("A");
+}else if(mark >= 60 && mark <= 79){
+  console.log("B");
+}else if(mark >= 40 && mark <= 59){
+  console.log("C");
+}else if(mark >= 20 && mark <= 39){
+  console.log("D");
+}else {
+  console.log("F");
+}
+
+
